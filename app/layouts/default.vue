7999f6004a94f18ae7474e6c50c128bde05bfe22
Alexandre Muhl <contact@alexandre-muhl.fr>
2026-04-17 03:39:33 +0200

    feat(shell): add default layout and hook it into app.vue

---
+++ app/layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const drawerOpen = ref(false)
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col">
+    <AppHeader :on-open-menu="() => drawerOpen = true" />
+    <div class="flex flex-1 min-h-0">
+      <AppSidebar />
+      <main class="flex-1 min-w-0 px-4 md:px-8 py-6 md:py-8">
+        <slot />
+      </main>
+    </div>
+    <AppMobileDrawer v-model="drawerOpen" />
+    <UToaster />
+  </div>
+</template>
